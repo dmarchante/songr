893bf7a7dc4774bf3e478cb49523bd58efcdc2d7
David Marchante <cdmarchante@gmail.com>
2019-06-11 16:10:14 -0700

    initialize repo

---
+++ src/main/java/com/dmarchante/code401d4/class12/firstWebApp/FirstWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.dmarchante.code401d4.class12.firstWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebAppApplication.class, args);
+	}
+
+}
